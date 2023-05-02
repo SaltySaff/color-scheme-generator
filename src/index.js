7c7aa0397d40f8d7858cc51382ace9fb1f72206c
saltysaff <d_ash12345@hotmail.co.uk>
2023-05-03 00:07:15 +0100

    added more functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 /* eslint-disable no-param-reassign */
-const renderColors = (colorsArray) => {
-  const colorOne = document.getElementById("color-one");
-  const colorTwo = document.getElementById("color-two");
-  const colorThree = document.getElementById("color-three");
-  const colorsElArray = [colorOne, colorTwo, colorThree];
 
-  colorsElArray.forEach((colorEl, index) => {
+const generateColorContainers = (count) => {
+  const containers = []
+  const colorsContainer = document.getElementById("colors-container")
+  let newDiv;
+
+  for (let i = 0; i < count; i += 1) {
+    newDiv = document.createElement('div');
+    newDiv.className = "color-container"
+    newDiv.id = `color${i + 1}`
+    containers.push(newDiv)
+    colorsContainer.appendChild(newDiv)
+  }
+  console.log(containers)
+  return containers
+};
+
+const getRandomHexColor = () => {
+    const hexChars = "0123456789ABCDEF"
+    let color = "#"
+
+    for (let i = 0; i < 6; i += 1) {
+        const randomIndex = Math.floor(Math.random() * 16)
+        color += hexChars[randomIndex]
+    }
+    return color
+}
+
+const colorContainers = generateColorContainers(5);
+
+const renderColors = (colorsArray) => {
+  colorContainers.forEach((colorEl, index) => {
     colorEl.style.backgroundColor = colorsArray[index];
   });
 };
@@ -20,7 +45,7 @@ const getColorsArray = (scheme) => {
 
 const fetchScheme = (color) => {
   fetch(
-    `https://www.thecolorapi.com/scheme?hex=${color}&format=json&mode=analogic&count=3`
+    `https://www.thecolorapi.com/scheme?hex=${color}&format=json&mode=analogic&count=5`
   )
     .then((res) => res.json())
     .then((scheme) => {
@@ -29,7 +54,11 @@ const fetchScheme = (color) => {
     });
 };
 
-const colorPicker = document.getElementById("color-picker");
-colorPicker.addEventListener("input", (e) => {
-  fetchScheme(e.target.value.substring(1));
+const getSchemeBtn = document.getElementById("get-scheme-btn");
+const colorPicker = document.getElementById("color-picker")
+fetchScheme(getRandomHexColor().substring(1))
+
+getSchemeBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  fetchScheme(colorPicker.value.substring(1));
 });
